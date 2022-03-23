8a9e9c908d84126a60cc0874fc4f449d0a398c2a
Junior Oliveira <iscrimou@gmail.com>
2022-03-23 17:48:33 -0300

    Auto message

--- src/service/teste.js
+++ src/service/teste.js
@@ -1,3 +1,4 @@
+const { randomInt } = require('crypto');
 const express = require('express');
 const app = express();
 const http = require('http');
@@ -8,7 +9,7 @@ const io = new Server(server);
 const {Client, LocalAuth} = require('whatsapp-web.js')
 const client = new Client({
     authStrategy: new LocalAuth({
-        clientId: "1234"
+        clientId: "260"
     })
 })
 
@@ -16,16 +17,25 @@ app.get('/', (req, res) => {
     res.sendFile('index.html', { root: './src/theme' });
 });
 
-client.initialize();
-
 client.on('qr', (qr) => {
     // NOTE: This event will not be fired if a session is specified.
     console.log('QR RECEIVED', qr);
+    io.emit('qrcode', qr);
 });
 
 client.on('authenticated', () => {
     console.log('AUTHENTICATED');
 });
+
+client.on('ready', () => {
+    console.log('READY');
+});
+
+client.on('message', (msg) => {
+    console.log('message received', msg);
+});
+
+client.initialize();
 
 io.on('connection', (socket) => {
     console.log('a user connected');
@@ -34,10 +44,8 @@ io.on('connection', (socket) => {
 
 io.on('connection', (socket) => {
     socket.on('message', (msg) => {
-
         console.log('MESSAGE RECEIVED', msg);
-
-        client.emit('message_create', message);
+        client.sendMessage('5521990822019@c.us', msg);
 
         io.emit('message', msg);
     });
